1c92994dba9aa692e7261f7b212451c546e8218f
AbhisheikSharma <abhi1982.mca@gmail.com>
2025-01-25 10:47:04 -0500

    Pushing all my practice work. this was done with java 11 and maveninzed

--- src/test/java/browserfactory/BaseClass.java
+++ src/test/java/browserfactory/BaseClass.java
@@ -33,19 +33,19 @@ public class BaseClass {
 	@Parameters({"browsername","appurl"})
 	public void setUpBrowserAndApp(String browsername, String auturl) throws InterruptedException {
 		if(browsername.equalsIgnoreCase("chrome")) {
-		ChromeOptions opt = new ChromeOptions();
-		opt.addArguments("--headless");
-		opt.addArguments("--window-size=1920,1080");
-		opt.addArguments("--disable-gpu");
-		opt.addArguments("--no-sandbox");
+	//	ChromeOptions opt = new ChromeOptions();
+	//	opt.addArguments("--headless");
+	//	opt.addArguments("--window-size=1920,1080");
+	//	opt.addArguments("--disable-gpu");
+	//	opt.addArguments("--no-sandbox");
 		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver(opt);
+		driver = new ChromeDriver();
 		}
 		else if(browsername.equalsIgnoreCase("firefox")) {
-			FirefoxOptions fopt = new FirefoxOptions();
-			fopt.addArguments("--headless");
+	//		FirefoxOptions fopt = new FirefoxOptions();
+	//		fopt.addArguments("--headless");
 		WebDriverManager.firefoxdriver().setup();
-		driver = new FirefoxDriver(fopt);
+		driver = new FirefoxDriver();
 		}
 		frontierpage = PageFactory.initElements(driver, FrontierPage.class);
 		screenshots = new Screenshots(driver);
